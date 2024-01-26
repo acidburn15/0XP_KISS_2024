5f982b142fd2ccc9f05e2d0d670319284c531bba
acidburn15 <acidburn1505@gmail.com>
2024-01-27 00:52:21 +0100

    Note respawn et map suivi de camera

--- Assets/Scripts/PlayerRespawn.cs
+++ Assets/Scripts/PlayerRespawn.cs
@@ -7,6 +7,7 @@ public class PlayerRespawn : MonoBehaviour
 {
     [SerializeField] private string[] killingTags;
     [SerializeField] private Transform spawn;
+    [SerializeField] public GameManager gameManager;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +24,7 @@ public class PlayerRespawn : MonoBehaviour
     {
         if (killingTags.Contains(killingObject.tag))
         {
+            gameManager.RespawnNotes();
             transform.position = spawn.position;
             // ajouter une mort au compteur du game Manager
         }
