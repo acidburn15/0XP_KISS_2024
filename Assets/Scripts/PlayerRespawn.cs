5cbf89a737c3b474289fe522d70dae8f5991bc47
acidburn15 <acidburn1505@gmail.com>
2024-01-26 19:52:55 +0100

    Joueur respawn au point de départ quand il meurt

---
+++ Assets/Scripts/PlayerRespawn.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PlayerRespawn : MonoBehaviour
+{
+    [SerializeField] private string[] killingTags;
+    [SerializeField] private Transform spawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = spawn.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D killingObject)
+    {
+        if (killingTags.Contains(killingObject.tag))
+        {
+            transform.position = spawn.position;
+            // ajouter une mort au compteur du game Manager
+        }
+    }
+}
