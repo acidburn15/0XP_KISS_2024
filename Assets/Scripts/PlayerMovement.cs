0664005e828d1775e5fc79bf625aac9ab9060c7d
acidburn15 <acidburn1505@gmail.com>
2024-01-26 16:41:32 +0100

    Base du jeu

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float moveSpeed;
+    private bool top;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        InvertGravity();
+
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    private void Move()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        
+        Vector3 moveDirection = new Vector3(moveX, 0).normalized;
+
+        transform.position +=  moveDirection * (moveSpeed * Time.deltaTime);
+    }
+    
+    private void InvertGravity()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.gravityScale *= -1;
+            Rotation();
+        }
+    }
+
+    void Rotation()
+    {
+        if (top == false)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 180f);
+        }
+        else
+        {
+            transform.eulerAngles = Vector3.zero;
+        }
+        top = !top;
+    }
+}
