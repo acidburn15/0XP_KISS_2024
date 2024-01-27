74658718b129ed04a723ffc6db277d2097b9a585
acidburn15 <acidburn1505@gmail.com>
2024-01-27 23:35:44 +0100

    footsteps sound

--- Assets/Scripts/SoundFXManager.cs
+++ Assets/Scripts/SoundFXManager.cs
@@ -15,6 +15,23 @@ public class SoundFXManager : MonoBehaviour
         }
     }
 
+    public void PlayRandomSoundFXClip(AudioClip[] audioClips, Transform spawnTransform, float volume)
+    {
+        int rand = Random.Range(0, audioClips.Length);
+
+        AudioSource audioSource = Instantiate(soundFXObject, spawnTransform.position, Quaternion.identity);
+
+        audioSource.clip = audioClips[rand];
+
+        audioSource.volume = volume;
+
+        audioSource.Play();
+
+        float clipLength = audioSource.clip.length;
+
+        Destroy(audioSource.gameObject, clipLength);
+    }
+
     public void PlaySoundFXClip(AudioClip audioClip, Transform spawnTransform, float volume)
     {
         AudioSource audioSource = Instantiate(soundFXObject, spawnTransform.position, Quaternion.identity);
