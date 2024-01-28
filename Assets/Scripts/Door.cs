06e1e8d2dd7880538319e6a5c652a53f23ac9944
NIHICHLAGGGGGGGGGGGGGG <romain.hubermont2307@gmail.com>
2024-01-28 03:10:00 +0100

    Level1

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Door : MonoBehaviour
 {
@@ -34,7 +35,8 @@ public class Door : MonoBehaviour
         if (open)
         {
             Debug.Log("NEXT LEVEL");
-            // load prochaine scène;
+            SceneManager.LoadScene(0);
+            // load prochaine scï¿½ne;
         }
     }
 }
